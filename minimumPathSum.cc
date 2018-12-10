7d8358dc18fc4aac7e6313e3c31d6f703672febe
James Jae Won Yoon <JamesYoon@Jamess-Macbook-Pro.local>
2018-12-09 23:45:32 -0500

    Better solution using vector<vector<int>> instead of map<vector<int>, int> for hash, but still need a little bit of improvement

--- minimumPathSum.cc
+++ minimumPathSum.cc
@@ -1,8 +1,7 @@
 class Solution {
 public:
-    void minPathSumWithCache(vector<vector<int>>& grid, map<vector<int>, int>& minSumDict, int row, int col, int prevSum, const int& gridRows, const int& gridCols) {
-        vector<int> currentPos{row, col};
-        int& currentSum = minSumDict[currentPos];
+        void minPathSumWithCache(vector<vector<int>>& grid, vector<vector<int>>& minSumHash, int row, int col, int prevSum, const int& gridRows, const int& gridCols) {
+        int& currentSum = minSumHash[row][col];
         int otherSum = prevSum + grid[row][col];
         if (currentSum == 0) {
             currentSum = otherSum;
@@ -14,11 +13,18 @@ public:
         if (row+1 == gridRows && col+1 == gridCols) {
             return;
         } else {
+            int sumOfNextGrid;
             if (row + 1 < gridRows) {
-                minPathSumWithCache(grid, minSumDict, row+1, col, currentSum, gridRows, gridCols);
+                sumOfNextGrid = minSumHash[row+1][col];
+                if (sumOfNextGrid == 0 || sumOfNextGrid > currentSum) { // so if the next grid's sum is smaller than current, don't try
+                    minPathSumWithCache(grid, minSumHash, row+1, col, currentSum, gridRows, gridCols);
+                }
             }
             if (col + 1 < gridCols) {
-                minPathSumWithCache(grid, minSumDict, row, col+1, currentSum, gridRows, gridCols);
+                sumOfNextGrid = minSumHash[row][col+1];
+                if (sumOfNextGrid == 0 || sumOfNextGrid > currentSum) { // so if the next grid's sum is smaller than current, don't try
+                    minPathSumWithCache(grid, minSumHash, row, col+1, currentSum, gridRows, gridCols);
+                }
             }
         }
     }
@@ -27,10 +33,12 @@ public:
         if (grid.empty()) {
             return 0;
         }
-        map<vector<int>, int> minSumDict;
         int gridRows = grid.size();
         int gridCols = grid[0].size();
-        minPathSumWithCache(grid, minSumDict, 0, 0, 0, gridRows, gridCols);
-        return minSumDict[{gridRows-1,gridCols-1}];
+        
+        vector<vector<int>> minSumHash(gridRows, vector<int>(gridCols, 0)); // initialize with 0
+
+        minPathSumWithCache(grid, minSumHash, 0, 0, 0, gridRows, gridCols);
+        return minSumHash[gridRows-1][gridCols-1];
     }
 };
