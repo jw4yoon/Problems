532f6166732f0e5298c22f7396770fdb518bfa79
James Jae Won Yoon <JamesYoon@Jamess-Macbook-Pro.local>
2018-12-10 23:43:58 -0500

    Reduced condition checks, so all tests now pass but still pretty slow

--- minimumPathSum.cc
+++ minimumPathSum.cc
@@ -1,29 +1,24 @@
 class Solution {
 public:
-        void minPathSumWithCache(vector<vector<int>>& grid, vector<vector<int>>& minSumHash, int row, int col, int prevSum, const int& gridRows, const int& gridCols) {
-        int& currentSum = minSumHash[row][col];
-        int otherSum = prevSum + grid[row][col];
-        if (currentSum == 0) {
-            currentSum = otherSum;
-        } else {
-            if (currentSum > otherSum) {
-                currentSum = otherSum;
-            }
-        }
-        if (row+1 == gridRows && col+1 == gridCols) {
+    void minPathSumWithCache(vector<vector<int>>& grid, vector<vector<int>>& minSumHash, int row, int col, const int& gridRows, const int& gridCols, int minSumOnCurrentGrid) {
+        if (row+1 == gridRows && col+1 == gridCols) { // arrived at bottom right
             return;
         } else {
-            int sumOfNextGrid;
+            int otherSumOfNextGrid;
             if (row + 1 < gridRows) {
-                sumOfNextGrid = minSumHash[row+1][col];
-                if (sumOfNextGrid == 0 || sumOfNextGrid > currentSum) { // so if the next grid's sum is smaller than current, don't try
-                    minPathSumWithCache(grid, minSumHash, row+1, col, currentSum, gridRows, gridCols);
+                int& sumOfNextGrid = minSumHash[row+1][col];
+                otherSumOfNextGrid = minSumOnCurrentGrid + grid[row+1][col];
+                if (sumOfNextGrid == 0 || sumOfNextGrid > otherSumOfNextGrid) { // so if the next grid's sum is smaller than current, don't try
+                    sumOfNextGrid = otherSumOfNextGrid;
+                    minPathSumWithCache(grid, minSumHash, row+1, col, gridRows, gridCols, sumOfNextGrid);
                 }
             }
-            if (col + 1 < gridCols) {
-                sumOfNextGrid = minSumHash[row][col+1];
-                if (sumOfNextGrid == 0 || sumOfNextGrid > currentSum) { // so if the next grid's sum is smaller than current, don't try
-                    minPathSumWithCache(grid, minSumHash, row, col+1, currentSum, gridRows, gridCols);
+            if (col + 1 <gridCols) {
+                int& sumOfNextGrid = minSumHash[row][col+1];
+                otherSumOfNextGrid = minSumOnCurrentGrid + grid[row][col+1];
+                if (sumOfNextGrid == 0 || sumOfNextGrid > otherSumOfNextGrid) { // so if the next grid's sum is smaller than current, don't try
+                    sumOfNextGrid = otherSumOfNextGrid;
+                    minPathSumWithCache(grid, minSumHash, row, col+1, gridRows, gridCols, sumOfNextGrid);
                 }
             }
         }
@@ -37,8 +32,11 @@ public:
         int gridCols = grid[0].size();
         
         vector<vector<int>> minSumHash(gridRows, vector<int>(gridCols, 0)); // initialize with 0
+        
+        int minSumOnCurrentGrid = grid[0][0];
+        minSumHash[0][0] = minSumOnCurrentGrid;
 
-        minPathSumWithCache(grid, minSumHash, 0, 0, 0, gridRows, gridCols);
+        minPathSumWithCache(grid, minSumHash, 0, 0, gridRows, gridCols, minSumOnCurrentGrid);
         return minSumHash[gridRows-1][gridCols-1];
     }
 };
